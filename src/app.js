dd80c6ae98b772628d9b22d51272479545f39917
todolayla-git <todolayla@gmail.com>
2025-10-29 12:03:10 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// frases ejercicio 
+let who   = ['La casa', 'mi tio', 'mi hijo', 'el curso'];
+let action= ['come', 'orina', 'agrega', 'rompe'];
+let what  = ['la rurtina', 'mi ventana', 'el coche'];
+let when  = ['despues del gym', 'mientars duermo', 'cuando fui a correr', 'en la playa', 'mientras cantaba'];
+
+//funcion ejercicio layla
+function generarExcusa () {
+  const whoPart   = who[Math.floor(Math.random()*who.length)];
+  const actionPart= action[Math.floor(Math.random()*action.length)];
+  const whatPart  = what[Math.floor(Math.random()*what.length)];
+  const whenPart  = when[Math.floor(Math.random()*when.length)];
+  return `${whoPart} ${actionPart} ${whatPart} ${whenPart}`;
+}
+
+window.onload = function () {
+  document.getElementById("excusa").innerHTML = generarExcusa();
+  
 };
